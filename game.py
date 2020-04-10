54ca79d44d24f83d9d85cca17f39bd2b83e8c461
Martin Penchev <53950572+mnpenchev@users.noreply.github.com>
2020-04-10 16:50:16 +0100

    Add files via upload
    Fixed menu, few minor bug fixed, added health potions

--- game.py
+++ game.py
@@ -54,34 +54,34 @@ print(colored("Good luck!", 'red'))
 # Defining player hero - class, and attributes
 ## mainhero = {'name': 'Playername', 'attack_num': 2, 'attack_dice': 20, 'health': 100, 'AC': 10, 'init': 14, 'experience': 0, 'gold': 0,
 ##           'level': 1}
-fighter = {'name': 'Korgan', 'attack_num': 3, 'attack_dice': 10, 'max_health': 250, 'health': 250, 'AC': 15, 'dex': 16,
+fighter = {'name': 'Korgan', 'attack_num': 5, 'attack_dice': 15, 'max_health': 250, 'health': 250, 'AC': 15, 'dex': 16,
            'experience': 0,
-           'gold': 0,
+           'gold': 0, 'health_potion': 0,
            'level': 1}
 mage = {'name': 'Edwin', 'attack_num': 2, 'attack_dice': 10, 'max_health': 200, 'health': 200, 'AC': 12, 'dex': 15,
         'experience': 0,
-        'gold': 0,
+        'gold': 0, 'health_potion': 0,
         'level': 1}
 cleric = {'name': 'Viconia', 'attack_num': 2, 'attack_dice': 10, 'max_health': 200, 'health': 120, 'AC': 12, 'dex': 15,
           'experience': 0,
-          'gold': 0,
+          'gold': 0, 'health_potion': 0,
           'level': 1}
 rogue = {'name': 'Yoshimo', 'attack_num': 4, 'attack_dice': 10, 'max_health': 160, 'health': 100, 'AC': 14, 'dex': 18,
          'experience': 0,
-         'gold': 0,
+         'gold': 0, 'health_potion': 0,
          'level': 1}
 
 # picking up hero ******
 while True:
     delay_print("\nPick up a hero!\n")
     print("\n1) Fighter: ")
-    print("Name:", fighter['name'], "\nHealth:", fighter['health'], "/ Attack: 3d15 / Armor class", fighter['AC'])
+    print("Name:", fighter['name'], "\nHealth:", fighter['health'], "/ Attack: 5d15 / Armor class", fighter['AC'])
     print("\n2) Mage: ")
-    print("Name:", mage['name'], "\nHealth:", mage['health'], "/ Attack: 3d15 / Armor class", mage['AC'])
+    print("Name:", mage['name'], "\nHealth:", mage['health'], "/ Attack: 2d10 / Armor class", mage['AC'])
     print("\n3) Priest: ")
-    print("Name:", cleric['name'], "\nHealth:", cleric['health'], "/ Attack: 1d15 / Armor class", cleric['AC'])
+    print("Name:", cleric['name'], "\nHealth:", cleric['health'], "/ Attack: 2d10 / Armor class", cleric['AC'])
     print("\n4) Rogue: ")
-    print("Name:", rogue['name'], "\nHealth:", rogue['health'], "/ Attack: 3d15 / Armor class", rogue['AC'])
+    print("Name:", rogue['name'], "\nHealth:", rogue['health'], "/ Attack: 4d10 / Armor class", rogue['AC'])
     player_class = input("\nSelect your class: ")
 
     if player_class == "1":
@@ -110,7 +110,7 @@ while True:
 # LEVEL UP FUNCTION **************
 def level_up(player_class):
     player_class['max_health'] += dice(2, 10)
-    player_class['health'] = player_class['max_health']
+    # player_class['health'] = player_class['max_health']
     player_class['attack_dice'] += dice(1, 5)
     return player_class
 
@@ -121,9 +121,8 @@ def level_up(player_class):
 ##        mainhero['attack_dice'] += dice(1, 5)
 ##        return mainhero
 
-
-while True:
-    # picking up an opponent # GAME LOOP
+# picking up an opponent function
+def monster():
     monster1 = {'name': 'Ogre', 'attack_num': 2, 'attack_dice': 10, 'health': 150, 'max_health': 150, 'AC': 12,
                 'dex': 13,
                 'experience': 5, 'gold': dice(1, 10)}
@@ -147,134 +146,136 @@ while True:
           colored(player_class['health'], 'red'),
           colored('health', 'red'), player_class['experience'], 'experience', player_class['gold'], 'gold.')
 
-    enemy = randint(1, 5)
+    enemy = dice(1, 5)
     if enemy == 1:
         enemy = monster1
-        print("\nYou are fighting", enemy['name'], "with", colored(enemy['health'], "red"), colored("health!", "red"))
     if enemy == 2:
         enemy = monster2
-        print("\nYou are fighting", enemy['name'], "with", colored(enemy['health'], "red"), colored("health!", "red"))
     if enemy == 3:
         enemy = monster3
-        print("\nYou are fighting", enemy['name'], "with", colored(enemy['health'], "red"), colored("health!", "red"))
     if enemy == 4:
         enemy = monster4
-        print("\nYou are fighting", enemy['name'], "with", colored(enemy['health'], "red"), colored("health!", "red"))
     if enemy == 5:
         enemy = monster5
-        print("\nYou are fighting", enemy['name'], "with", colored(enemy['health'], "red"), colored("health!", "red"))
+    print("\nYou are fighting", enemy['name'], "with", colored(enemy['health'], "red"), colored("health!", "red"))
+    return enemy
 
 
-    def combat(a, b):
-        while True:
-            print("\nPlease select action:\n")
-            print("1) Normal attack")
-            print("2) Heavy attack")
-            print("3) Heal")
-            player_select = input()
+def combat(a, b):
+    while True:
+        print("\nPlease select action:\n")
+        print("1) Normal attack")
+        print("2) Heavy attack")
+        print("3) Heal")
+        player_select = input()
 
-            if player_select == "1":
-                if dice(1, 20) + a['dex'] - 7 > b['AC']:
-                    b['health'] = b['health'] - dice(a['attack_num'], a['attack_dice'])
-                    print(clear)
-                    print(a['name'], "attacked the", b['name'], "for", dice(a['attack_num'], a['attack_dice']),
-                          "damage")
-                else:
-                    print(clear)
-                    print(a['name'], "missed!")
-
-            elif player_select == "2":
-                if dice(1, 20) + a['dex'] - 10 > b['AC']:
-                    b['health'] = b['health'] - dice(round(a['attack_num'] / 2), a['attack_dice'] + 10)
-                    print(clear)
-                    print(a['name'], "attacked the", b['name'], "for",
-                          dice(round(a['attack_num'] / 2), a['attack_dice'] + 10), "damage")
-                else:
-                    print(clear)
-                    print(a['name'], "missed!")
-
-            elif player_select == "3":
-                a['health'] = a['health'] + dice(a['attack_num'], a['attack_dice'])
-                # ***** NO OVERHEAL ALLOWED! ****
-                if a['health'] + dice(a['attack_num'],
-                                      a['attack_dice']) > a['max_health']:
-                    a['health'] = a['max_health']
+        if player_select == "1":
+            if dice(1, 20) + a['dex'] - 7 > b['AC']:
+                b['health'] = b['health'] - dice(a['attack_num'], a['attack_dice'])
                 print(clear)
-                print(a['name'], "healed for", dice(a['attack_num'], a['attack_dice']),
-                      "health")
+                print(a['name'], "attacked the", b['name'], "for", dice(a['attack_num'], a['attack_dice']),
+                      "damage")
             else:
                 print(clear)
-                print(colored("Invalid input, please enter option from the menu.", 'red'))
-                continue
+                print(a['name'], "missed!")
 
-            if b['health'] <= 0:
-                print("You have defeated", b['name'], "and received", b['experience'], "experience and", enemy['gold'],
-                      "gold\n")
-                a['experience'] = a['experience'] + b['experience']
-                a['gold'] = a['gold'] + b['gold']
-                a['health'] = a['health'] + dice(5, 10)
-                print(a['name'], "restored", dice(5, 10), "health after the fight\n")
-                if a['health'] + dice(a['attack_num'], a['attack_dice']) > a['max_health']:
-                    a['health'] = a['max_health']
-                if a['experience'] >= 10:
-                    a['level'] += 1
-                    print(colored("Congratulations! You reached level", "blue"), colored(a['level'], "blue"),
-                          colored("!", "blue"))
-                    a = level_up(a)
-                    a['experience'] = 0
-                break
-
-            if b['health'] >= (b['max_health'] / 2):
-                enemy_select = randint(1, 2)
+        elif player_select == "2":
+            if dice(1, 20) + a['dex'] - 10 > b['AC']:
+                b['health'] = b['health'] - dice(round(a['attack_num'] / 2), a['attack_dice'] + 10)
+                print(clear)
+                print(a['name'], "attacked the", b['name'], "for",
+                      dice(round(a['attack_num'] / 2), a['attack_dice'] + 10), "damage")
             else:
-                enemy_select = randint(1, 3)
+                print(clear)
+                print(a['name'], "missed!")
 
-            if enemy_select == 1:
-                if dice(1, 20) + b['dex'] - 7 > a['AC']:
-                    a['health'] = a['health'] - dice(b['attack_num'], b['attack_dice'])
-                    print(b['name'], "did", dice(b['attack_num'], b['attack_dice']), "damage!\n")
-                else:
-                    print(b['name'], "missed!")
+        elif player_select == "3":
+            a['health'] = a['health'] + dice(a['attack_num'], a['attack_dice'])
+            # ***** NO OVERHEAL ALLOWED! ****
+            if a['health'] + dice(a['attack_num'],
+                                  a['attack_dice']) > a['max_health']:
+                a['health'] = a['max_health']
+            print(clear)
+            print(a['name'], "healed for", dice(a['attack_num'], a['attack_dice']),
+                  "health")
+        else:
+            print(clear)
+            print(colored("Invalid input, please enter option from the menu. - (combat menu)", 'red'))
+            continue
 
-            elif enemy_select == 2:
-                if dice(1, 20) + b['dex'] - 10 > a['AC']:
-                    a['health'] = a['health'] - dice(round(b['attack_num'] / 2),
-                                                     b['attack_dice'] + 10)
-                    print(b['name'], "did", dice(round(b['attack_num'] / 2), b['attack_dice'] + 10), "damage!\n")
-                else:
-                    print(b['name'], "missed!")
+        if b['health'] <= 0:
+            print("You have defeated", b['name'], "and received", b['experience'], "experience and", b['gold'],
+                  "gold\n")
+            a['experience'] = a['experience'] + b['experience']
+            a['gold'] = a['gold'] + b['gold']
+            b['health'] = b['max_health']
+            if a['health'] + dice(a['attack_num'], a['attack_dice']) > a['max_health']:
+                a['health'] = a['max_health']
+            if a['experience'] >= 10:
+                a['level'] += 1
+                print(colored("Congratulations! You reached level", "blue"), colored(a['level'], "blue"),
+                      colored("!", "blue"))
+                a = level_up(a)
+                a['experience'] = 0
+            break
 
-            elif enemy_select == 3:
-                b['health'] = b['health'] + dice(b['attack_num'], b['attack_dice'])
-                if b['health'] + dice(b['attack_num'], b['attack_dice']) > b['max_health']: b['health'] = b[
-                    'max_health']
-                print(b['name'], "healed for", dice(b['attack_num'], b['attack_dice']), "health\n")
+        if b['health'] >= (b['max_health'] / 2):
+            enemy_select = randint(1, 2)
+        else:
+            enemy_select = randint(1, 3)
 
-            # display health after every round
-            if b['health'] >= 0 and a['health'] >= 0:
-                do_health(a['name'], a['health'], a['max_health'])
-                do_health(b['name'], b['health'], b['max_health'])
+        if enemy_select == 1:
+            if dice(1, 20) + b['dex'] - 7 > a['AC']:
+                a['health'] = a['health'] - dice(b['attack_num'], b['attack_dice'])
+                print(b['name'], "did", dice(b['attack_num'], b['attack_dice']), "damage!\n")
+            else:
+                print(b['name'], "missed!")
 
-            # in case player loose the fight, the game is over.
-            if a['health'] <= 0:
-                print(a['name'], "have been defeated.")
-                delay_print(colored("\nYou lost your live! \n **************** GAME OVER ****************", "red"))
-                break
+        elif enemy_select == 2:
+            if dice(1, 20) + b['dex'] - 10 > a['AC']:
+                a['health'] = a['health'] - dice(round(b['attack_num'] / 2),
+                                                 b['attack_dice'] + 10)
+                print(b['name'], "did", dice(round(b['attack_num'] / 2), b['attack_dice'] + 10), "damage!\n")
+            else:
+                print(b['name'], "missed!")
 
-    combat(player_class, enemy)
-    break
+        elif enemy_select == 3:
+            b['health'] = b['health'] + dice(b['attack_num'], b['attack_dice'])
+            if b['health'] + dice(b['attack_num'], b['attack_dice']) > b['max_health']: b['health'] = b[
+                'max_health']
+            print(b['name'], "healed for", dice(b['attack_num'], b['attack_dice']), "health\n")
+
+        # display health after every round
+        if b['health'] >= 0 and a['health'] >= 0:
+            do_health(a['name'], a['health'], a['max_health'])
+            do_health(b['name'], b['health'], b['max_health'])
+
+        # in case player loose the fight, the game is over.
+        if a['health'] <= 0:
+            print(a['name'], "have been defeated.")
+            delay_print(colored("\nYou lost your live! \n **************** GAME OVER ****************", "red"))
+            quit()
+
+
+combat(player_class, monster())
 
 while True:
+    count = 0
+    count1 = 0
     print("\nWhat would you want to do next?\n")
-    print("1) Scavenge the area: ")
-    print("2) Continue to the next area: ")
+    print("1) Scavenge: ")
+    print("2) Search for enemy: ")
     print("3) Go back in town: ")
-    print("4) Leave the game: ")
+    if player_class['health_potion'] > 0:
+        print("4) Drink a health potion - restores 1d20 health, you have", player_class['health_potion'], "potions.")
+    else:
+        print("4) You have 0 potions")
+    print("5) Leave the game: ")
     player_choice = input()
 
     if player_choice == "1":
         print("\nSearch the area for gold and items\n")
-        search = randint(1, 3)
+        search = randint(1, 4)
         if search == 1:
             gold_find = randint(1, 10)
             print(clear)
@@ -288,21 +289,34 @@ while True:
             continue
         if search == 3:
             print(clear)
-            print("Another enemy appeared, prepare to fight!")
-            combat(player_class, enemy)
+            print("Enemy appeared, prepare to fight!")
+            combat(player_class, monster())
+            continue
+        if search == 4:
+            print(clear)
+            print("You found a health potion")
+            player_class['health_potion'] += 1
             continue
     if player_choice == "2":
         print(clear)
         print("You travel trough the dessert, after few hour of walking an enemy appeared!\n Prepare for combat.\n")
-        combat(player_class, enemy)
+        combat(player_class, monster())
         continue
     if player_choice == "3":
         print(clear)
         print("Heading back to town to restore your health")
         player_class['health'] = player_class['max_health']
-        print(player_class['name'], "restored")
+        print(player_class['name'], "health was fully restored.")
         continue
     if player_choice == "4":
-        break
+        if player_class['health_potion'] > 0:
+            player_class['health'] += dice(1, 20)
+            if player_class['health'] > player_class['max_health']:
+                player_class['health'] = player_class['max_health']
+            player_class['health_potion'] -= 1
+            print(player_class['name'], "restored", dice(1, 20), "health.")
+            continue
+    if player_choice == "5":
+        quit()
     else:
-        print(colored("\nPlease select a valid choice.", "red"))
+        print(colored("\nPlease select a valid input - (game menu).", "red"))
